7c7f4354bb6bbb93512be3bfce7e89a269d5a9b1
DESKTOP-KS461A2\Papouboy <corentin.trehioy@wanadoo.fr>
2017-10-08 18:19:49 +0200

    Modification Faim, déplacement en diagonale

--- PigeonSquare/Strategie/Faim.cs
+++ PigeonSquare/Strategie/Faim.cs
@@ -15,33 +15,34 @@ namespace PigeonSquare.Strategie
 
         public override void Deplacement(int dimX, int dimY, Pigeon unPerso)
         {
-            if(unPerso.target.X != unPerso.X )
-            {
-                if(unPerso.target.X > unPerso.X)
+ 
+            if (unPerso.target.X != unPerso.X)
+                 {
+                     if (unPerso.target.X > unPerso.X)
+                     {
+                            unPerso.X += 1;
+                     }
+                     else
+                     {
+                         unPerso.X -= 1;
+                     }
+                 }
+
+            if (unPerso.target.Y != unPerso.Y)
                 {
-                    unPerso.X += 1;
-                }
-                else 
-                {
-                    unPerso.X -= 1;
-                }
-            }else if (unPerso.target.Y != unPerso.Y)
-            {
-                if (unPerso.target.Y > unPerso.Y)
-                {
-                    unPerso.Y += 1;
-                }
-                else
-                {
-                    unPerso.Y -= 1;
-                }
-            }
-            else
+                    if (unPerso.target.Y > unPerso.Y)
+                    {
+                        unPerso.Y += 1;
+                    }
+                    else
+                    {
+                        unPerso.Y -= 1;
+                    }
+                 }
+            if (unPerso.target.Y == unPerso.Y && unPerso.target.X == unPerso.X)
             {
                 unPerso.mange();
             }
-
-           
         }
     }
 }
